fed9f5ec36fbc5aece5ee7ed9946286b1106e384
HenryKanyoro <kanyorohenry6@gmail.com>
2020-10-12 00:45:40 +0300

    improved

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
     $(".moto").click(function () {
-         $(".moto").hide(550);
-         $(".fly").show(550);
+         $(".moto").hide(750);
+         $(".fly").show(750);
     });
     $(".fly").click(function () {
-         $(".fly").hide(550);
-         $(".moto").show(550);
+         $(".fly").hide(750);
+         $(".moto").show(750);
     });
     $(".tamu").click(function () {
-         $(".tamu").hide(550);
-         $(".cup").show(550);
+         $(".tamu").hide(750);
+         $(".cup").show(750);
     });
     $(".cup").click(function () {
-         $(".cup").hide(550);
-         $(".tamu").show(550);
+         $(".cup").hide(750);
+         $(".tamu").show(750);
     });
     $(".plate").click(function () {
-         $(".plate").hide(550);
-         $(".kijiko").show(550);
+         $(".plate").hide(750);
+         $(".kijiko").show(750);
     });
     $(".kijiko").click(function () {
-         $(".kijiko").hide(550);
-         $(".plate").show(550);
+         $(".kijiko").hide(750);
+         $(".plate").show(750);
     });
 });
 
